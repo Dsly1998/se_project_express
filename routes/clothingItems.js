6dd9a038fc22266ad3da34d2751b165a26e0fe6c
Dallin Sly <krave83@gmail.com>
2023-10-05 17:00:21 -0400

    finish

--- routes/clothingItems.js
+++ routes/clothingItems.js
@@ -4,10 +4,12 @@ const router = express.Router();
 const clothingItemsController = require("../controllers/clothingItems");
 const authMiddleware = require("../middlewares/auth"); // Import the auth middleware
 
+// Route for getting all items without protection
+router.get("/", clothingItemsController.getAllItems);
+
 // Protect all subsequent routes with the auth middleware
 router.use(authMiddleware);
 
-router.get("/", clothingItemsController.getAllItems);
 router.post("/", clothingItemsController.createItem);
 router.delete("/:itemId", clothingItemsController.deleteItem);
 router.put("/:id/likes", clothingItemsController.likeItem);
