0044f5d0247bcf7b53eb397c129d377afc7ded88
Dallin Sly <krave83@gmail.com>
2024-02-04 21:46:10 -0500

    validation issue

--- routes/clothingItems.js
+++ routes/clothingItems.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const clothingItemsController = require("../controllers/clothingItems");
 const authMiddleware = require("../middlewares/auth"); // Import the auth middleware
+const { validateCardBody } = require("../middlewares/validation");
 
 // Route for getting all items without protection
 router.get("/", clothingItemsController.getAllItems);
@@ -9,7 +10,7 @@ router.get("/", clothingItemsController.getAllItems);
 // Protect all subsequent routes with the auth middleware
 router.use(authMiddleware);
 
-router.post("/", clothingItemsController.createItem);
+router.post("/", validateCardBody, clothingItemsController.createItem);
 router.delete("/:itemId", clothingItemsController.deleteItem);
 router.put("/:id/likes", clothingItemsController.likeItem);
 router.delete("/:id/likes", clothingItemsController.dislikeItem);
